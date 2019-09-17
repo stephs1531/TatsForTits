04fbfae349bb931ece7f713bf85d555ba3c5989f
Steph Sapp <stephs1531@gmail.com>
2019-09-17 08:56:26 -0500

    finish why tats component

--- my-app/src/components/WhyTats.js
+++ my-app/src/components/WhyTats.js
@@ -8,17 +8,21 @@ class WhyTats extends React.Component {
 
         return (
             <div className="row why-tats valign-wrapper">
-                <div className="article s12 m5">
+                <div className="article col s12 m5">
                     <div className="headline">
                         <h1 className="tats-headline">why tattoos?</h1>
                     </div>
                     <div className="paragraph">
                         <p>We want to enable more women to get tattoos to cover their scars by creating a travelling tattoo parlor. The truck will travel across the United States for the entire 2018 year. Tattoo artists will travel with the truck to provide quality, professional tattoos to help breast cancer survivors heal and put cancer behind them.</p>
                     </div>
+                    <div className="button-container">
+                    <button className="button">testimonies</button>
+                    </div>
                 </div>
-                <div className="article-image s12 m5">
-                    <img src={Image} className="article-image"/>
+                <div className="article-image col s12 m5">
+                    <img src={Image}/>
                 </div>
+               
             </div>
         )
         }
